e07a7f8e79f2cd58202611ce6ac63188ebec9c7c
A---- <A----@users.noreply.github.com>
2015-01-28 12:35:19 +0100

    Removing commented copy/pasted lines

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,59 +53,6 @@ module.exports = function(grunt) {
           }
       }
     }
-
-	/*copy: {
-		jquery: {
-			expand: true,
-			cwd: 'node_modules/jquery/dist',
-			src: 'jquery*',
-			dest: 'dist/lib/jquery/'
-		},
-		"bootstrap-js": {
-			expand: true,
-			cwd: 'node_modules/bootstrap/dist/js/',
-			src: 'bootstrap*',
-			dest: 'dist/lib/bootstrap/'
-		},
-		"bootstrap-glyphicon": {
-			expand: true,
-			cwd: 'node_modules/bootstrap/dist/fonts/',
-			src: '*',
-			dest: 'dist/fonts/glyphicon/'
-		},
-		"bootstrap-datepicker": {
-			expand: true,
-			cwd: 'node_modules/bootstrap-datepicker/js/',
-			src: 'bootstrap-datepicker*',
-			dest: 'dist/lib/bootstrap-datepicker/'
-		},
-		"highlight-js": {
-			expand: true,
-			cwd: 'lib/highlight.js/',
-			src: 'highlight*',
-			dest: 'dist/lib/highlight.js/'
-		},
-		"highlight-css": {
-			expand: true,
-			cwd: 'lib/highlight.js/styles/',
-			src: 'default.css',
-			dest: 'dist/lib/highlight.js/'
-		},
-		"font-awesome": {
-			expand: true,
-			cwd: 'node_modules/font-awesome/fonts/',
-			src: '*',
-			dest: 'dist/fonts/font-awesome/'
-		},
-		"sg-bootstrap-js": {
-			expand: true,
-			cwd: 'js/',
-			src: '*',
-			dest: 'dist/js/'
-		}
-	}*/
-
-
   });
 
   grunt.registerTask('default', ['clean', 'uglify', 'copy']);
